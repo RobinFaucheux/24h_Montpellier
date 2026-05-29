4f7e0435a869e1281c35c63d6c7f953f5e14f923
RobinFaucheux <robin.faucheux@etu.univ-orleans.fr>
2026-05-29 22:00:13 +0200

    update

--- web/server/utils/stripe.ts
+++ web/server/utils/stripe.ts
@@ -4,8 +4,11 @@ let _stripe: Stripe | null = null
 
 export function useStripe(): Stripe {
   if (!_stripe) {
-    const config = useRuntimeConfig()
-    _stripe = new Stripe(config.stripeSecretKey, { apiVersion: '2025-05-28.basil' })
+    const key = process.env.STRIPE_SECRET_KEY
+    if (!key) {
+      throw new Error('STRIPE_SECRET_KEY environment variable is not set')
+    }
+    _stripe = new Stripe(key, { apiVersion: '2025-05-28.basil' })
   }
   return _stripe
 }
